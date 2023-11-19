0c06b8cc342c7c6caf28b07bd25bf8ab9634d8cb
nopain8269 <146239597+nopain8269@users.noreply.github.com>
2023-11-19 23:56:23 +0100

    Update server.lua
    update to work with latest esx

--- server.lua
+++ server.lua
@@ -1,11 +1,11 @@
 ESX  = nil
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = exports["es_extended"]:getSharedObject()
 
 ESX.RegisterUsableItem('cigarette', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
     local smoke = xPlayer.getInventoryItem('cigarette')
 
     xPlayer.removeInventoryItem('cigarette', 1)
-    TriggerClientEvent('esx_cigarette:startSmoke', source)
+    TriggerClientEvent('esx_cigarette:useCigarette', source)
 end)
